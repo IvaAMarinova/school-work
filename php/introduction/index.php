afb1917eef4be69a7801a35da2ee77171099f736
IvaAMarinova <ivamarinova06@gmail.com>
2024-09-30 15:22:25 +0300

    Added first php code

---
+++ php/introduction/index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title> PHP Example </title>
+
+    <style>
+        body {
+        background-color: linen;
+        }
+
+        h1 {
+        color: pink;
+        }
+    </style>
+</head>
+
+<body>
+    <h1> Example Hello </h1>
+    <p>
+        <?php
+            echo 'Hello';
+        ?>
+    </p>
+</body>
+</html>
